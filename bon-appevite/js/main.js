6ed02196eb11501b896a105392448daa6a452ffa
whydoesthecodenotwork <lawrencel54@nycstudents.net>
2023-10-30 13:14:33 -0400

    Use more website

--- bon-appevite/js/main.js
+++ bon-appevite/js/main.js
@@ -6,9 +6,9 @@ html.insertAdjacentHTML(
 	`<link rel="stylesheet" href="styles/style.css">`
 );
 
-document
-	.querySelector('input[type="range"')
-	.addEventListener("input", updateFilter);
+// document
+// 	.querySelector('input[type="range"')
+// 	.addEventListener("input", updateFilter);
 
 function updateFilter(event) {
 	try {
@@ -17,18 +17,129 @@ function updateFilter(event) {
 		// we ran this function outside of an event listener... too bad!
 	}
 	app.replaceChildren();
-	const hp = document.querySelector("label");
-	hp.querySelector("em").innerText =
-		hp.querySelector("input[type='range'").value;
-	products.forEach((product) => {
-		app.insertAdjacentHTML(
-			"beforeend",
-			`
-					<h1>this is ${product}. here is a picture</h1>
-					<img src="images/${product.image}"/>		
-			`
-		);
-	});
+	products.forEach((product) => addCard(product));
 }
 
 updateFilter();
+
+function addCard(product) {
+	{
+		insertCard(product); // insert card with all fields filled
+		updateCard(app.lastElementChild, product); // go in and edit the fields that aren't needed and other stuff
+	}
+}
+
+function insertCard(product) {
+	if (product.hasOwnProperty("types")) {
+		var item = product.types[0]; // const has block scope... too bad!
+	} else {
+		var item = product;
+	}
+	app.insertAdjacentHTML(
+		"beforeend",
+		`
+		<div class="card">
+		<h2>${product.name}</h2>
+		<s><h4>$${item.price}</h4></s>
+			<h3>$${item.discounted}</h3>
+			<img src="images/${item.image}" alt="great product" />
+			<div class="rating">
+				<h3>${item.rating} stars</h3>
+				<div class="stars">
+				
+				</div>
+				</div>
+			<button onclick="alert('you shall not buy!!')">
+			BUY NOW!!
+			</button>
+		</div>	
+		`
+	);
+
+	// we're adding the dropdown and its event listener here
+	// because adding it in the updateCard was recursion
+	const card = app.lastElementChild;
+
+	if (
+		(card.querySelector("select") == null) &
+		product.hasOwnProperty("types")
+	) {
+		// add a select element if it doesn't already exist
+		const select = document.createElement("select");
+		product.types.forEach((type) =>
+			select.insertAdjacentHTML(
+				"beforeend",
+				`<option value="${type.type}">${type.type}</option>`
+			)
+		);
+		select.children[0].selected = true;
+		card.querySelector("button").insertAdjacentElement(
+			"beforebegin",
+			select
+		);
+		card.querySelector("select").addEventListener(
+			"onchange",
+			updateCard(card, product)
+		);
+	}
+}
+
+function updateCard(card, product) {
+	if (card.querySelector("select") !== null) {
+		var item = product.types[card.querySelector("select").selectedIndex];
+		card.innerHTML = `
+							<h2>${product.name}</h2>
+							<s><h4>$${item.price}</h4></s>
+								<h3>$${item.discounted}</h3>
+								<img src="images/${item.image}" alt="great product" />
+								<div class="rating">
+									<h3>${item.rating} stars</h3>
+									<div class="stars">
+									
+									</div>
+									</div>
+								<button onclick="alert('you shall not buy!!')">
+								BUY NOW!!
+								</button>
+						`;
+	} else {
+		var item = product;
+	}
+
+	// console.log(item);
+	// If the item has no price, label it as out of stock
+	if (isNaN(item.price)) {
+		card.querySelector("h4").innerText = "This item is out of stock.";
+		card.querySelector("button").remove();
+	}
+
+	// If the item has no discount, make the discount element contain the price.
+	// This is because the price is an h4 with strikethrough, so if we just deleted the discount then the price would still be an h4 with strikethrough.
+	if (isNaN(item.discounted)) {
+		card.querySelector("h3").innerText = card.querySelector("h4").innerText;
+		card.querySelector("s").remove();
+	}
+
+	// Do horrible things to create a ratings bar.
+	// Actual stars are overrated anyway
+	const stars = card.querySelector(".stars");
+	// Items with dropdown will just keep adding stars
+	stars.replaceChildren();
+	var rating = item.rating;
+	for (let i = 0; i < 5; i++) {
+		stars.insertAdjacentHTML(
+			"beforeend",
+			`						
+					<div class="star">
+						<div class="filled" style="width: 0%"></div>
+						<div class="empty"></div>
+					</div>
+					`
+		);
+		if (rating > 0) {
+			stars.lastElementChild.children[0].style.width =
+				Math.min(1, rating) * 100 + "%";
+			rating -= 1;
+		}
+	}
+}

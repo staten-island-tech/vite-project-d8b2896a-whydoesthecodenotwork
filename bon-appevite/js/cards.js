99638fce322ab4158d351cf1696a035c3a7c24b5
dell <91681777+whydoesthecodenotwork@users.noreply.github.com>
2023-10-30 11:45:09 -0400

    Use more website

--- bon-appevite/js/cards.js
+++ bon-appevite/js/cards.js
@@ -1,13 +1,49 @@
 const products = [
-	{
-		name: "Dr. Breen's private reserve",
-		price: 1.99,
-		discounted: 0.99,
-		rating: 5,
-		image: "breen.png",
-		description:
-			"Drink the water. We put nothing in it to make you forget.",
-	},
+    {
+        name: "Dr. Breen's private reserve",
+        price: 1.99,
+        discounted: 0.99,
+        rating: 5,
+        image: "breenblue.jpg",
+        description: "The finest water in all of City 17.",
+    },
+    {
+        name: "Gelatinated Calorie Pastes",
+        price: 3.99,
+        rating: 1.3,
+        image: "paste.jpg",
+        description: "Egg flavored",
+    },
+    {
+        name: "Desiccated Sustenance Bars",
+        price: 2.99,
+        rating: 1.5,
+        image: "bar.jpg",
+        description: "Water flavored",
+    },
+    {
+        name: "Standard Ration",
+        price: 5.99,
+        discounted: 5.98,
+        rating: 1.4,
+        image: "ration.jpg",
+        description: "Standard citizen ration",
+    },
+    {
+        name: "Melon",
+        price: 99.99,
+        discounted: 99.98,
+        rating: 5,
+        image: "melon.jpg",
+        description: "Melon",
+    },
+    {
+        name: "Beer",
+        price: NaN,
+        rating: 5,
+        image: "beer.jpg",
+        description: "Melon",
+    },
 ];
 
 export { products };
